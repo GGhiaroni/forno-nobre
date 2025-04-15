a4e7941338594f9c53e31a15deaae282f6057dbc
GGhiaroni <gghiaronitiziano@gmail.com>
2025-04-15 13:23:21 -0300

    comecando a definir os estilos globais

---
+++ src/GlobalStyle/index.jsx
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+:root {
+  font-family: "Work Sans", sans-serif;
+  font-optical-sizing: auto;
+
+  --tamanho-fonte-nome-pizza: 1.25rem;
+  --tamanho-fonte-categoria-pizza: .75rem;
+  --tamanho-fonte-descricao-pizza: .875rem;
+  --peso-fonte-nome-pizza: 600;
+  --peso-fonte-descricao-pizza: 400;
+  --peso-fonte-preco-pizza: 700;
+  --tamanho-fonte-l: 2.5rem;
+  --tamanho-fonte-m: 1.125rem;
+  
+
+  --cor-branca: #FFFFFF;
+  --cor-primaria: #D22418;
+  --cor-cinza-claro: #64748B;
+  --cor-cinza-medio: #94A3B8;
+  --cor-cinza-escuro: #1E293B;
+  --cor-laranja: #F87415;
+  --cor-amarelo: #FAE08D;
+  --cor-background-input: #F7FAFC;
+  --cor-icone-carrinho: #020817;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  background-color: var(--cor-branca);
+}
+
+`;
+
+export default GlobalStyle;
