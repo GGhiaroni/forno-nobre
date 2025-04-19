c7646168833d41bff61dda87215dd96cb1079d56
GGhiaroni <gghiaronitiziano@gmail.com>
2025-04-19 10:44:29 -0300

    ajustando mais alguns pontos da pagina de detalhes da pizza

--- src/Pages/Pizza.jsx
+++ src/Pages/Pizza.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LuPizza } from "react-icons/lu";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
@@ -29,6 +29,7 @@ const ContainerDetalhes = styled.div`
 
 const ImagemPizza = styled.img`
   width: 600px;
+  height: 400px;
   border-radius: 12px;
   object-fit: cover;
 `;
@@ -44,6 +45,22 @@ const ContainerMaisFotos = styled.div`
     height: auto;
     object-fit: cover;
     box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+  }
+`;
+
+const Miniatura = styled.img`
+  border-radius: 8px;
+  width: 100px;
+  height: auto;
+  object-fit: cover;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  border: 3px solid transparent;
+  transition: border 0.3s ease;
+
+  &.selecionada {
+    border: 3px solid var(--cor-primaria);
   }
 `;
 
@@ -112,29 +129,37 @@ const BtnAdicionar = styled.button`
   }
 `;
 
+const ContainerImagensPizza = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const Pizza = () => {
   const [pizzaEncontrada, setPizzaEncontrada] = useState();
   const [loading, setLoading] = useState(true);
+  const [fotoPizzaSelecionada, setFotoPizzaSelecionada] = useState(1);
 
   const parametro = useParams();
 
-  fetch(
-    `https://6801135281c7e9fbcc416de2.mockapi.io/fornonobre/pizzas/${parametro.id}`
-  )
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Pizza não encontrada.");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      setPizzaEncontrada(data);
-      setLoading(false);
-    })
-    .catch((error) => {
-      console.error("Erro:", error);
-      setLoading(false);
-    });
+  useEffect(() => {
+    fetch(
+      `https://6801135281c7e9fbcc416de2.mockapi.io/fornonobre/pizzas/${parametro.id}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Pizza não encontrada.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPizzaEncontrada(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Erro:", error);
+        setLoading(false);
+      });
+  }, [parametro.id]);
 
   if (loading) return <p>Carregando pizza...</p>;
 
@@ -142,23 +167,54 @@ const Pizza = () => {
 
   const ingredientesSeparados = ingredientes.split(",");
 
+  const selecionarFoto = (id) => {
+    setFotoPizzaSelecionada(id);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <ContainerPagina>
       <ContainerPrincipal>
-        <div>
-          <ImagemPizza src={pizzaEncontrada.fotoUm} />
+        <ContainerImagensPizza>
+          <ImagemPizza
+            src={
+              fotoPizzaSelecionada === 1
+                ? pizzaEncontrada.fotoUm
+                : fotoPizzaSelecionada === 2
+                ? pizzaEncontrada.fotoDois
+                : pizzaEncontrada.fotoTres
+            }
+          />
           <ContainerMaisFotos>
             {pizzaEncontrada.fotoUm && (
-              <img src={pizzaEncontrada.fotoUm} alt="Foto extra 1" />
+              <Miniatura
+                src={pizzaEncontrada.fotoUm}
+                alt={`Foto 1 pizza ${pizzaEncontrada.sabor}`}
+                onClick={() => selecionarFoto(1)}
+                className={fotoPizzaSelecionada === 1 ? "selecionada" : ""}
+              />
             )}
             {pizzaEncontrada.fotoDois && (
-              <img src={pizzaEncontrada.fotoDois} alt="Foto extra 2" />
+              <Miniatura
+                src={pizzaEncontrada.fotoDois}
+                alt={`Foto 2 pizza ${pizzaEncontrada.sabor}`}
+                onClick={() => selecionarFoto(2)}
+                className={fotoPizzaSelecionada === 2 ? "selecionada" : ""}
+              />
             )}
             {pizzaEncontrada.fotoTres && (
-              <img src={pizzaEncontrada.fotoTres} alt="Foto extra 3" />
+              <Miniatura
+                src={pizzaEncontrada.fotoTres}
+                alt={`Foto 3 pizza ${pizzaEncontrada.sabor}`}
+                onClick={() => selecionarFoto(3)}
+                className={fotoPizzaSelecionada === 3 ? "selecionada" : ""}
+              />
             )}
           </ContainerMaisFotos>
-        </div>
+        </ContainerImagensPizza>
         <ContainerDetalhes>
           <Categoria>{pizzaEncontrada.categoria}</Categoria>
           <H3Estilizado>{pizzaEncontrada.sabor}</H3Estilizado>
