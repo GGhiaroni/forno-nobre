a0607e03cdeca7f69c3e61f3bef8b24723aaf82d
GGhiaroni <gghiaronitiziano@gmail.com>
2025-04-24 08:29:21 -0300

    icone de carrinho quanto contem itens ja esta mostrando o numero de itens

--- src/Componentes/Carrinho/index.jsx
+++ src/Componentes/Carrinho/index.jsx
@@ -1,10 +1,39 @@
+import { observer } from "mobx-react-lite";
 import { IoCartSharp } from "react-icons/io5";
-const Carrinho = () => {
+import styled from "styled-components";
+import { useStoreContext } from "../../mobx/StoreContext";
+
+const CarrinhoContainer = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const Badge = styled.span`
+  position: absolute;
+  top: -8px;
+  right: -8px;
+  background-color: var(--cor-primaria);
+  color: white;
+  font-size: 0.75rem;
+  font-weight: bold;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Carrinho = observer(() => {
+  const { carrinhoStore } = useStoreContext();
   return (
-    <>
+    <CarrinhoContainer>
       <IoCartSharp size={30} color="#333" />
-    </>
+      {carrinhoStore.totalItensNoCarrinho > 0 && (
+        <Badge>{carrinhoStore.totalItensNoCarrinho}</Badge>
+      )}
+    </CarrinhoContainer>
   );
-};
+});
 
 export default Carrinho;
