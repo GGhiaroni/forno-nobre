d55fac517d352af588455db7ca708cb530fb019d
GGhiaroni <gghiaronitiziano@gmail.com>
2025-04-25 06:34:26 -0300

    criando a rota de carrinho e comecano a criar a pagina

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "sonner";
+import Carrinho from "./Componentes/Carrinho";
 import Footer from "./Componentes/Footer";
 import Header from "./Componentes/Header";
 import Home from "./Pages/Home";
@@ -14,6 +15,7 @@ function AppRoutes() {
         <Route index element={<Home />} />
         <Route path="/pizza/:id/:sabor" element={<Pizza />} />
         <Route path="/categorias/:categoria" element={<PizzasPorCategoria />} />
+        <Route path="/carrinho" element={<Carrinho />} />
       </Routes>
       <Footer />
       <Toaster position="top-right" richColors />
