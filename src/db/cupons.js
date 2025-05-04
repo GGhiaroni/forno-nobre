a387c2889659f923dd74f73790d6502020669c70
GGhiaroni <gghiaronitiziano@gmail.com>
2025-05-04 11:50:40 -0300

    criando a base de cupons

---
+++ src/db/cupons.js
@@ -0,0 +1,36 @@
+const cupons = [
+  {
+    codigo: "PIZZA10",
+    valor: 10,
+  },
+  {
+    codigo: "FORNO5",
+    valor: 5,
+  },
+  {
+    codigo: "FRETEGRATIS",
+    valor: 0,
+  },
+  {
+    codigo: "PIZZA15",
+    valor: 15,
+  },
+  {
+    codigo: "BLACKFRIDAY",
+    valor: 30,
+  },
+  {
+    codigo: "FORNO20",
+    valor: 20,
+  },
+  {
+    codigo: "BEMVINDO10",
+    valor: 10,
+  },
+  {
+    codigo: "PROMO25",
+    valor: 25,
+  },
+];
+
+export default cupons;
