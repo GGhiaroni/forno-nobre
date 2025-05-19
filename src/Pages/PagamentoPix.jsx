37bda0c1813565e493e834fd7ac47303cf6ccc88
GGhiaroni <gghiaronitiziano@gmail.com>
2025-05-19 18:53:47 -0300

    finalizando o layout da página de pagamento por pix

--- src/Pages/PagamentoPix.jsx
+++ src/Pages/PagamentoPix.jsx
@@ -5,6 +5,8 @@ import QRCode from "react-qr-code";
 import { Link } from "react-router-dom";
 import { toast } from "sonner";
 import styled from "styled-components";
+import ResumoPedido from "../Componentes/ResumoPedido";
+import { useStoreContext } from "../mobx/StoreContext";
 
 const ContainerPrincipal = styled.div`
   width: 100%;
@@ -84,21 +86,31 @@ const ContainerQrCode = styled.div`
   padding: 1.5rem;
   border-radius: 8px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.08);
+  max-width: 80%;
 `;
 
 const ContainerCards = styled.div`
   display: flex;
+  gap: 2rem;
 `;
 
-const ParagrafoEstilizado = styled.div`
+const ParagrafoEstilizado = styled.span`
   font-size: 1.3rem;
   font-weight: 600;
   color: var(--cor-cinza-escuro);
 `;
 
-const ParagrafoEstilizadoSecundario = styled.div`
+const ParagrafoEstilizadoSecundario = styled.span`
   font-size: 1rem;
   color: var(--cor-cinza-medio);
+  margin-bottom: 1rem;
+`;
+
+const ParagrafoEstilizadoInstrucoes = styled.span`
+  font-size: 1rem;
+  color: var(--cor-cinza-medio);
+  margin-bottom: 0.5rem;
+  font-weight: 300;
 `;
 
 const CodigoPix = styled.div`
@@ -123,6 +135,7 @@ const BotaoCopiar = styled.button`
   font-family: "Poppins";
   font-size: 1rem;
   color: var(--cor-cinza-medio);
+  margin-bottom: 2rem;
 
   &:hover {
     background-color: var(--cor-cinza-claro-extra);
@@ -136,15 +149,44 @@ const ContainerQrCodeImagem = styled.div`
   padding: 1rem;
   border-radius: 8px;
   max-width: fit-content;
+  margin-bottom: 1rem;
 `;
 
 const ContainerInstrucoes = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: flex-start;
+  align-self: flex-start;
+  width: 100%;
+`;
+
+const BotaoFinalizar = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 10px;
+  padding: 14px;
+  border: none;
+  border-radius: 10px;
+  background: var(--cor-primaria);
+  color: #fff;
+  font-size: 1rem;
+  font-family: "Poppins", sans-serif;
+  font-weight: 600;
+  width: 100%;
+  cursor: pointer;
+  transition: background 0.3s;
+  &:hover {
+    background: var(--cor-hover);
+  }
 `;
 
 const PagamentoPix = () => {
   const [codigoPix, setCodigoPix] = useState("");
+  const { carrinhoStore } = useStoreContext();
+  const [codigoCupom, setCodigoCupom] = useState("");
+  const [cupomAplicado, setCupomAplicado] = useState(null);
 
   useEffect(() => {
     const gerarCodigoPix = () => {
@@ -205,27 +247,43 @@ const PagamentoPix = () => {
             Copiar código
           </BotaoCopiar>
           <ContainerInstrucoes>
-            <ParagrafoEstilizado>Como pagar com PIX</ParagrafoEstilizado>
-            <ParagrafoEstilizadoSecundario>
-              1. Abra o aplicativo do seu banco
-            </ParagrafoEstilizadoSecundario>
-            <ParagrafoEstilizadoSecundario>
-              2. Escolha a opção PIX
-            </ParagrafoEstilizadoSecundario>
-            <ParagrafoEstilizadoSecundario>
-              3. Escaneie o QR Code ou cole o código copiado
-            </ParagrafoEstilizadoSecundario>
-            <ParagrafoEstilizadoSecundario>
-              4. Confirme as informações e valor
-            </ParagrafoEstilizadoSecundario>
-            <ParagrafoEstilizadoSecundario>
-              5. Finalize o pagamento no aplicativo do seu banco
-            </ParagrafoEstilizadoSecundario>
-            <ParagrafoEstilizadoSecundario>
-              6. Após pagar, clique em "Confirmar Pagamento" abaixo
-            </ParagrafoEstilizadoSecundario>
+            <ParagrafoEstilizado style={{ marginBottom: "1rem" }}>
+              Como pagar com PIX
+            </ParagrafoEstilizado>
+            <ParagrafoEstilizadoInstrucoes>
+              <b style={{ fontWeight: "500" }}>1.</b> Abra o aplicativo do seu
+              banco
+            </ParagrafoEstilizadoInstrucoes>
+            <ParagrafoEstilizadoInstrucoes>
+              <b style={{ fontWeight: "500" }}>2.</b> Escolha a opção PIX
+            </ParagrafoEstilizadoInstrucoes>
+            <ParagrafoEstilizadoInstrucoes>
+              <b style={{ fontWeight: "500" }}>3.</b> Escaneie o QR Code ou cole
+              o código copiado
+            </ParagrafoEstilizadoInstrucoes>
+            <ParagrafoEstilizadoInstrucoes>
+              <b style={{ fontWeight: "500" }}>4.</b> Confirme as informações e
+              valor
+            </ParagrafoEstilizadoInstrucoes>
+            <ParagrafoEstilizadoInstrucoes>
+              <b style={{ fontWeight: "500" }}>5.</b> Finalize o pagamento no
+              aplicativo do seu banco
+            </ParagrafoEstilizadoInstrucoes>
+            <ParagrafoEstilizadoInstrucoes>
+              <b style={{ fontWeight: "500" }}>6.</b> Após pagar, clique em
+              "Confirmar Pagamento" abaixo
+            </ParagrafoEstilizadoInstrucoes>
           </ContainerInstrucoes>
+          <BotaoFinalizar>Confirmar pagamento</BotaoFinalizar>
         </ContainerQrCode>
+        <ResumoPedido
+          carrinhoStore={carrinhoStore}
+          exibirBotao={false}
+          codigoCupom={codigoCupom}
+          setCodigoCupom={setCodigoCupom}
+          cupomAplicado={cupomAplicado}
+          setCupomAplicado={setCupomAplicado}
+        />
       </ContainerCards>
     </ContainerPrincipal>
   );
